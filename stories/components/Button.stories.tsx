f660a28d14db66087de2a75d9fa47e1d88c8b627
fathrahh <gazoon3@gmail.com>
2022-07-26 01:57:11 +0700

    postcss addon

---
+++ stories/components/Button.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Button from "../../components/Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Base = Template.bind({});
+
+Base.args = {
+  children: "Hello World",
+};
