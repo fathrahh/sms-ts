1823f375e7f6600d6b8d99bc2fce3fbe78c8ffc6
fathrahh <gazoon3@gmail.com>
2022-07-30 18:49:17 +0700

    Add Ripple Effect for Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type SizeProps = 'small' | 'medium' | 'large' | 'full';
 
@@ -6,13 +6,13 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: () => void;
   className?: string;
   size?: SizeProps;
-  style?: React.CSSProperties;
+  variant?: 'primary' | 'secondary';
   children: React.ReactNode;
 }
 function matchSize(size: SizeProps): string {
   switch (size) {
     case 'small':
-      return '140px';
+      return '120px';
     case 'medium':
       return '170px';
     case 'large':
@@ -22,24 +22,60 @@ function matchSize(size: SizeProps): string {
   }
 }
 
+type RippleProp = {
+  posX: number | string;
+  posY: number | string;
+};
+function Ripple({ posX, posY }: RippleProp) {
+  return (
+    <span
+      className="absolute bg-white h-4 w-4"
+      style={{
+        left: posX,
+        top: posY,
+      }}
+    />
+  );
+}
+
 export default function Button({
-  onClick,
+  onClick = () => {},
   className = '',
   children,
   size = 'full',
+  variant = 'primary',
   style,
   ...other
 }: Props) {
   let width = matchSize(size);
 
+  const [rippleEffect, setRippleEffect] = useState<RippleProp[]>([]);
+  const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onClick();
+    const _button = event.currentTarget;
+    let posX = event.clientX - _button.offsetLeft;
+    let posY = event.clientY - _button.offsetTop;
+    let ripplePos = { posX, posY };
+    setRippleEffect([...rippleEffect, ripplePos]);
+    setTimeout(() => {
+      const temporaryRipple = [...rippleEffect];
+      temporaryRipple.pop();
+      setRippleEffect([...temporaryRipple]);
+    }, 3000);
+  };
+
   return (
     <button
-      onClick={onClick}
+      onClick={handleOnClick}
       style={{ width, ...style }}
-      className={`${className} bg-slate-600 text-white`}
+      className={`${className} relative py-2 bg-slate-600 text-white`}
       {...other}
     >
       {children}
+      {rippleEffect &&
+        rippleEffect.map(({ posX, posY }, idx) => (
+          <Ripple key={idx} posX={posX} posY={posY} />
+        ))}
     </button>
   );
 }
