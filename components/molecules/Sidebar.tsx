f2f4e42efd20c07e5a37e0c53f417acd1630e93e
fathrahh <gazoon3@gmail.com>
2022-08-23 12:30:59 +0700

    Sidebar

---
+++ components/molecules/Sidebar.tsx
@@ -0,0 +1,62 @@
+import { Scrollbars } from 'react-custom-scrollbars-2';
+import { sidebarProps } from '../../utils/consts/SidebarProps';
+
+import { SidebarLink } from '../atoms';
+
+export default function Sidebar() {
+  // Responsive state
+  //   const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Scrollbars
+      autoHideDuration={100}
+      universal={true}
+      autoHide
+      className="max-w-md w-[90vw] lg:max-w-[300px] lg:w-full h-full bg-white overflow-y-auto z-50 absolute hidden lg:relative lg:block border-r border-gray-300"
+    >
+      <div className="py-8">
+        <h1 className="text-4xl font-bold text-orange text-center pb-12 border-b-2 border-muted">
+          ToqCer
+        </h1>
+        <div className="select-none">
+          {sidebarProps.map((sidebarProp, index) => (
+            <ul key={index} className="py-4 border-b-2 border-muted">
+              {sidebarProp.children.map((level1, index) =>
+                level1?.children ? (
+                  <SidebarLink
+                    key={index}
+                    text={level1.text}
+                    Icon={level1.icon}
+                    href={level1.href}
+                  >
+                    <ul
+                      className={`m-0 transition-all duration-300 overflow-hidden`}
+                    >
+                      {level1.children.map((level2) => (
+                        <SidebarLink
+                          text={level2.text}
+                          key={Math.random()}
+                          href={level2.href}
+                          Icon={level2.icon}
+                          className="py-2"
+                        />
+                      ))}
+                    </ul>
+                  </SidebarLink>
+                ) : (
+                  <SidebarLink
+                    text={level1.text}
+                    href={level1.href}
+                    key={Math.random()}
+                    Icon={level1.icon}
+                    className="py-3"
+                  />
+                ),
+              )}
+            </ul>
+          ))}
+        </div>
+      </div>
+    </Scrollbars>
+  );
+}
